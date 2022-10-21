bced7bbae1059127df3ad9baab4f5c4c4ff15dda
BobWritesCode <warwick_hart@hotmail.com>
2022-10-21 23:40:54 +0100

    Toggle duty and availabilty. Set user details

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const btnPdDashboard = document.getElementById('btnPdDashboard');
 const btnPdIncidents = document.getElementById('btnPdIncidents');
 
 window.addEventListener('DOMContentLoaded', function() {
+  assignUser()
   btnPdDashboard.addEventListener("click", function() {
     changeToPdDashboard();
   });
@@ -14,10 +15,14 @@ window.addEventListener('DOMContentLoaded', function() {
   });
 
 });
+const btnDuty = document.getElementById('btn-duty');
+btnDuty.addEventListener("click", toggleDuty);
 
+const btnOfficerAvailable = document.getElementById('btn-officer-available');
+btnOfficerAvailable.addEventListener("click", toggleOfficerAvailable);
 
 /**
- * 
+ * Change MDT to Dashboard screen
  */
  function changeToPdDashboard() {
   pdDasboardCont.style.display = "flex";
@@ -25,9 +30,47 @@ window.addEventListener('DOMContentLoaded', function() {
  }
 
  /**
- * 
+ * Change MDT to Incident screen
  */
 function changeToPdIncidents() {
   pdDasboardCont.style.display = "none";
   pdIncidentCont.style.display = "flex";
+}
+
+/**
+ * Toogle on and off duty
+ */
+function toggleDuty() {
+  if (btnDuty.textContent == "On") {
+    btnDuty.textContent = "Out";
+    btnDuty.style.backgroundColor = "red";
+  } else {
+    btnDuty.textContent = "On";
+    btnDuty.style.backgroundColor = "green";
+  }
+}
+
+/**
+ * Toggle availabilty
+ */
+function toggleOfficerAvailable() {
+  if (btnOfficerAvailable.textContent == "Yes") {
+    btnOfficerAvailable.textContent = "No";
+    btnOfficerAvailable.style.backgroundColor = "red";
+  } else {
+    btnOfficerAvailable.textContent = "Yes";
+    btnOfficerAvailable.style.backgroundColor = "green";
+  }
+}
+
+/**
+ * Assign user
+ */
+ function assignUser() {
+  const spanUserName = document.getElementById("user-name");
+  const spanUserRank = document.getElementById("user-rank");
+  const spanUserCallsign = document.getElementById("user-callsign");
+  spanUserName.textContent = "Graves"
+  spanUserRank.textContent = "Captain"
+  spanUserCallsign.textContent = "315"
 }
