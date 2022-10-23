817f56f53c36f162016179a4bccc3bd246e9a15f
BobWritesCode <warwick_hart@hotmail.com>
2022-10-23 15:40:36 +0100

    Improve search function
    - If attr match found then add,
    - unless another input does not match.
    - Ignore user inputs left blank.

--- assets/script.js
+++ assets/script.js
@@ -10,34 +10,37 @@ const btnCallSign = doc.getElementById('btn-call-sign');
 const btnPopupSubmit = doc.getElementById('popup-btn-submit');
 const btnOfficerAvailable = doc.getElementById('btn-officer-available');
 const btnAddEvent = doc.getElementById('btn-add-event');
+const btnPerformPDSearch = doc.getElementById('pd-search-button');
+
 
 window.addEventListener('DOMContentLoaded', function() {
   assignUser()
 
+	// Navigation function calls
 	btnPdDashboard.addEventListener("click", function() {
 		changeMdtScreen.makeChange('pd-dashboard-container');
 	});
-
 	btnPdSearch.addEventListener("click", function() {
 		changeMdtScreen.makeChange('pd-search-container');
 	});
-	
 	btnPdIncidents.addEventListener("click", function() {
 		changeMdtScreen.makeChange('pd-incident-container');
 	});
 
+	// Dashboard function calls
   btnDuty.addEventListener("click", toggleDuty);
-
+	btnOfficerAvailable.addEventListener("click", toggleOfficerAvailable);
   btnCallSign.addEventListener("click", function() {
     funcPopup("Call Sign");
   });
-
   btnPopupSubmit.addEventListener("click", function() {
     funcPopup.popupSubmit();
 	});
 
-  btnOfficerAvailable.addEventListener("click", toggleOfficerAvailable);
+	// PD search function calls
+	btnPerformPDSearch.addEventListener("click", performPdSearch);
 
+	// PD incidents function calls
   btnAddEvent.addEventListener("click", addEventToEventHistory);
 
 });
@@ -105,10 +108,8 @@ function toggleOfficerAvailable() {
  function funcPopup(r_reason) {
   const popup = doc.getElementById("popup");
   var reason = r_reason
-
   h2 = popup.getElementsByTagName("h2");
   h2[0].innerHTML = reason;
-
   if (popup.style.display != "block") {
 
     popup.style.display = "block";
@@ -120,18 +121,15 @@ function toggleOfficerAvailable() {
     let input1 = doc.getElementById("popup-input1");
     var data = input1.value;
     popup.style.display = "none";
-
     switch(reason) {
       case "Call Sign":
         changeCallSign(data)
         break;
       default:
-        // code block
     }
   }
 
   funcPopup.popupSubmit = popupSubmit;
-
 }
 
 /**
@@ -200,7 +198,7 @@ function toggleOfficerAvailable() {
                 +   "<p>Inc: " + incNum + "</p>"
                 +   "<p>Loc: " + incLoc + "</p>"
                 +   "<p>Info: " + incInf + "</p>"
-                + "</div>"
+                + "</div>";
   const incidentCol = doc.getElementById("pd-dashboard-most-wanted");
   incidentCol.innerHTML += data;
 }
@@ -221,6 +219,66 @@ function toggleOfficerAvailable() {
   incidentCol.innerHTML += data;
   doc.getElementById("txt-add-event").value = "";
 }
+
+/**
+ * Perform search on the PD MDT Search screen
+ */
+function performPdSearch() {
+	var user = [];
+	user[0] = doc.getElementById('pd-search-fname').value;
+	user[1] = doc.getElementById('pd-search-sname').value;
+	user[2] = doc.getElementById('pd-search-dob').value;
+	user[3] = doc.getElementById('pd-search-address').value;
+
+	// Set up table header
+	const searchResults = doc.getElementById("search-results");
+	searchResults.innerHTML =	"<tr>"
+														+ "<th>Name</th>"
+														+ "<th>DOB</th>"
+														+ "<th>Address</th>"
+														+ "<th>Flags</th>"
+														+ "</tr>"; 
+
+	// Search each row 1 by 1
+	tableData = fakePeople;
+	for (let i=0; i < tableData.length; i++) {
+		var addToList = false;
+		var key = tableData[i]
+		console.log(key);
+
+		// Check each user inout for blank
+		// true continue
+		// false check against person attr, if match add to list
+		for (let j=0; j < user.length; j++) {
+			if (user[j] == "") { continue }
+				var a = key[j].toLowerCase();
+				var b = user[j].toLowerCase();
+			if (a.includes(b)) {	
+				addToList = true;
+			 } else {
+				addToList = false;
+				break;
+			 }
+		};
+
+		// If potential match add to search results
+		if (addToList) {
+			var newRow =	"<tr>"
+										+ "<td>" + tableData[i][0] + " " + tableData[i][1] + "</td>"
+										+ "<td>" + tableData[i][2] + "</td>"
+										+ "<td>" + tableData[i][3] + "</td>"
+										+ "<td>" +  + "</td>"
+										+ "<td>	<button id='btn-goto-person'>\>\>\></button>	</td>"
+										+ "</tr>";
+			searchResults.innerHTML += newRow;
+		};
+	
+	};
+};
+
+const fakePeople = [];
+fakePeople[0] = ['George', 'Bones', '12/01/85', '7 Park Lane'];
+fakePeople[1] = ['Gary', 'Bones', '27/09/88', '6 Park Lane'];
 
 changeMdtScreen()
 
